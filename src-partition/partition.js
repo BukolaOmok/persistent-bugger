e52921b4444bbe415fd78e5d91af6fde05bc08de
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-06-12 10:57:19 +0100

    add file and test

---
+++ src-partition/partition.js
@@ -0,0 +1,14 @@
+function partition (arr, callback) {
+    // const passedArray = []
+    // const failedArray = []
+
+    // for element of arr
+    //     if the element passes the callback
+    //         push element into passedArray
+    //     else 
+    //         push into failedArray
+    // return [passedArray, failedArray]
+}
+
+export {partition}
+
