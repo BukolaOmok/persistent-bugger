0ebdea3adada8f67945d5a88a09fec079684801c
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-06-21 10:55:21 +0100

    add fuction for splitting numbers to array

--- src/persistence.js
+++ src/persistence.js
@@ -2,12 +2,17 @@ function persistence(num) {
   let count = 0;
   let newNum = num;
 
+  function splitNumbersToArray(newNum) {
+    let splitDigits = Array.from(String(newNum), Number);
+    return splitDigits;
+  }
+
   if (num < 10) {
     return 0;
   }
 
   while (newNum > 9) {
-    let arrayOfDigits = Array.from(String(newNum), Number);
+    let arrayOfDigits = splitNumbersToArray(newNum);
     let multiplyDigits = 1;
     for (let i of arrayOfDigits) {
       multiplyDigits *= i;
